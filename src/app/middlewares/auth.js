d705e4dc9de926d7d7b9525fe4812dbf24372e6a
= <=>
2019-10-20 17:19:36 -0300

    chore: Removed comments on file src>middlewares>auth.js

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -10,12 +10,9 @@ export default async (req, res, next) => {
     return res.status(401).json({ error: 'Token not provided.' });
   }
 
-  // O array possui duas posições ('Bearer [hash do token]'). Da maneira abaixo, descartamos o 'Bearer'
-  // e fazemos com que o hash do token seja a primeira e única posição
   const [, token] = authHeader.split(' ');
 
   try {
-    // o payload do token está na const encoded ( id do usuário e data de expiração)
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
